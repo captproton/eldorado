fdc0697cddfdcebb99ed74bfe300434e17429d6c
Carl Tanner <captproton@betterbilling.net>
2009-07-23 17:26:23 -0600

    renamed files with event to happening

--- db/migrate/050_add_location_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddLocationToHappenings < ActiveRecord::Migration
-  def self.up
-    add_column :happenings, :location, :text
-  end
-
-  def self.down
-    remove_column :happenings, :location
-  end
-end
--- db/migrate/059_remove_events_location.rb
+++
@@ -1,9 +0,0 @@
-class RemoveHappeningsLocation < ActiveRecord::Migration
-  def self.up
-    remove_column :happenings, :location
-  end
-
-  def self.down
-    raise IrreversibleMigration
-  end
-end
