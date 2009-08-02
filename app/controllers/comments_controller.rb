f5c961bfb0557a30466c6e02e6adf1ae256c5459
Carl Tanner <captproton@betterbilling.net>
2009-08-02 10:08:44 -0700

    Added fxml and xml formats to comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,32 @@ class CommentsController < ApplicationController
   before_filter :can_edit, :only => [:edit, :update, :destroy]
   
   def index
-    @comments = Comment.paginate(:page => params[:page], :order => 'created_at desc', :include => [:begetter, :user])
+    
+    respond_to do |format|
+      format.html {
+        @comments = Comment.paginate(:page => params[:page], 
+          :order => 'created_at desc', :include => [:begetter, :user])
+      }
+      format.xml  { 
+        @comments = Comment.paginate(:page => params[:page], 
+          :order => 'created_at desc', :include => [:begetter, :user])
+        render :xml => @comments 
+      }
+      format.fxml  { 
+        @comments = Comment.find(:all)
+        render :fxml => @comments.to_fxml(:include => [:begetter, :user])
+      }
+    end
   end
   
   def show
     @comment = Comment.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @comment }
+      format.fxml  { render :fxml => @comment }
+    end
   end
   
   def new
@@ -21,25 +42,44 @@ class CommentsController < ApplicationController
   
   def create
     @comment = current_user.comments.build(params[:comment])
-    if @comment.save
-      redirect_to polymorphic_path(@comment.begetter) + '#c' + @comment.id.to_s
-    else
-      redirect_to polymorphic_path(@comment.begetter) + '#comments'
+    
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to polymorphic_path(@comment.begetter) + '#c' + @comment.id.to_s }
+        format.xml  { render :xml => @comment, :status => :created, :location => @workunit }
+        format.fxml  { render :fxml => @comment }
+      else
+        format.html { redirect_to polymorphic_path(@comment.begetter) + '#comments' }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        format.fxml  { render :fxml => @comment.errors }
+      end
     end
   end
   
   def update
     @comment = Comment.find(params[:id])
-    if @comment.update_attributes(params[:comment])
-      redirect_to polymorphic_path(@comment.begetter) + '#c' + @comment.id.to_s
-    else
-      render :action => "edit"
+    
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to polymorphic_path(@comment.begetter) + '#c' + @comment.id.to_s }
+        format.xml  { head :ok }
+        format.fxml  { render :fxml => @comment }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        format.fxml  { render :fxml => @comment.errors }
+      end
     end
   end
   
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to polymorphic_path(@comment.begetter) + '#comments'
+    
+    respond_to do |format|
+      format.html { redirect_to polymorphic_path(@comment.begetter) + '#comments' }
+      format.xml  { head :ok }
+      format.fxml  { render :fxml => @comment }
+    end
   end
 end
